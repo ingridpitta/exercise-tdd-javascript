a5477d7d6018e36ba0b853e6c94591b56d0bac5f
ingridpitta <ingridpitta056@gmail.com>
2020-07-15 22:08:21 -0300

    queue [done]

--- queue.js
+++ queue.js
@@ -1,5 +1,23 @@
 function createQueue() {
-
+  return {
+    elements: [],
+    size() {
+      return this.elements.length
+    },
+    add(e) {
+      this.elements.push(e)
+      return this.size()
+    },
+    peek() {
+      return this.elements[0]
+    },
+    dequeue() {
+      if (!this.size()) {
+        throw new Error()
+      }
+      return this.elements.shift()
+    },
+  }
 }
 
 module.exports = { createQueue }
